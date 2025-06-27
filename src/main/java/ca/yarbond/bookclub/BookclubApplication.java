291b82d32f053fa21c21e1904a58f2bf0c0fa343
Yaroslav Bondarenko <yaroslavbon@gmail.com>
2025-06-26 21:43:58 -0400

    Added dotenv

--- src/main/java/ca/yarbond/bookclub/BookclubApplication.java
+++ src/main/java/ca/yarbond/bookclub/BookclubApplication.java
@@ -1,5 +1,6 @@
 package ca.yarbond.bookclub;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -9,6 +10,15 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class BookclubApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure()
+				.ignoreIfMalformed()
+				.ignoreIfMissing()
+				.load();
+		
+		dotenv.entries().forEach(entry -> {
+			System.setProperty(entry.getKey(), entry.getValue());
+		});
+		
 		SpringApplication.run(BookclubApplication.class, args);
 	}
 
